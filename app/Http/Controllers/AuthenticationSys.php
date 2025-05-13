fe058ee10569d27c3bc154800959e8a52a6b6cce
BradRice2202 <bradley.rice2202@gmail.com>
2025-05-13 13:37:30 +0200

    initial commit. Laravel setup and basic configuration.

---
+++ app/Http/Controllers/AuthenticationSys.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthenticationSys extends Controller
+{
+    // Function to log users into site.
+
+    function login()
+    {
+
+    }
+
+    function loginWithPost(Request $request)
+    {
+
+    }
+
+    function register()
+    {
+
+    }
+
+    function registerPost(Request $request)
+    {
+
+    }
+}
